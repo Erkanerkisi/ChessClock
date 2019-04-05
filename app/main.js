c4e9763d5ef530ab3fb20ef7a83af2bb1b5b845e
Erkan <erkanerkisi@gmail.com>
2019-04-05 10:27:54 +0300

    still adjusting and adding style stuff

--- app/main.js
+++ app/main.js
@@ -1,47 +1,57 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View,Button} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default class main extends Component{
-    render(){
+export default class main extends Component {
+    render() {
         return (
             <View style={{flex: 1, padding: 10}}>
-              <View style={styles.text}>
-                    <Text></Text>
-              </View>
-              <View style={styles.settings}>
+                <View style={[{ flex: 1, backgroundColor: '#808080', borderRadius: 20 }]}>
+                    <View style={styles.text}>
+                        <Text style={{fontSize: 90}}>Erkan</Text>
+                    </View>
+                </View>
+
+                <View style={styles.settings}>
                     <Text>Settings</Text>
+                </View>
+
+
+
+                <View style={[{ flex: 1, backgroundColor: '#808080', borderRadius: 20 }]}>
+                    <View style={styles.text}>
+                        <Text>Erkan</Text>
+                    </View>
+                </View>
             </View>
-              <View style={styles.text2}>
-                    <Text></Text>
-            </View>
-            </View>
-          );
+        );
     }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F31A1A',
-  },
-  text: {
-    flex: 2,
-    backgroundColor: '#F31A1A',
-    borderRadius: 20,
-    padding: 10
-  },
-  text2: {
-    flex: 2,
-    backgroundColor: '#49D265',
-    borderRadius: 20,
-    padding: 10
-  },
-  settings: {
-    flex: 0.5,
-    backgroundColor: '#013CFE',
-    padding: 10
-  }
+    container: {
+        padding: 0,
+        margin: 0,
+        display: 'flex'
+    },
+    text: {
+        flex: 2,
+        backgroundColor: '#808080',
+        borderRadius: 20,
+        padding: 10,
+        margin: 10,
+
+    },
+    text2: {
+        flex: 2,
+        backgroundColor: '#49D265',
+        padding: 10,
+        borderRadius: 20,
+        margin: 10
+    },
+    settings: {
+        flex: 0.5,
+        backgroundColor: '#013CFE',
+        padding: 10
+    }
 });
