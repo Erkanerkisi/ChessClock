cde30cf3610ab1cd35d789cdb0b44a283929da1a
Erkan <erkanerkisi@gmail.com>
2019-04-15 22:21:32 +0300

    added logic

--- app/main.js
+++ app/main.js
@@ -1,57 +1,94 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TouchableWithoutFeedback } from 'react-native';
+import CountDown from 'react-native-countdown-component';
 
 export default class main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstTimerRunning: false,
+            secondTimerRunning: false,
+            min: 5,
+            sec: 0
+        };
+    }
+
     render() {
         return (
-            <View style={{flex: 1, padding: 10}}>
-                <View style={[{ flex: 1, backgroundColor: '#808080', borderRadius: 20 }]}>
-                    <View style={styles.text}>
-                        <Text style={{fontSize: 90}}>Erkan</Text>
-                    </View>
-                </View>
+            <View >
+                <TouchableWithoutFeedback onPress={this.tickFirstTimer.bind(this)}>
+                    <View style={[styles.tabs, styles.rotate180, this.state.firstTimerRunning ? styles.active : styles.passive]}>
+                        <CountDown
+                            until={60 * this.state.min + this.state.sec}
+                            size={100}
+                            digitStyle={this.state.firstTimerRunning ? styles.active : styles.passive}
+                            digitTxtStyle={this.state.firstTimerRunning ? styles.active : styles.passive}
+                            timeToShow={['M', 'S']}
+                            timeLabels={{ m: '', s: '' }}
+                            running={this.state.firstTimerRunning}
 
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
                 <View style={styles.settings}>
                     <Text>Settings</Text>
                 </View>
+                <TouchableWithoutFeedback onPress={this.tickSecondTimer.bind(this)}>
+                    <View style={[styles.tabs, this.state.secondTimerRunning ? styles.active : styles.passive]}>
+                        <CountDown
+                            until={60 * this.state.min + this.state.sec}
+                            size={100}
+                            digitStyle={this.state.secondTimerRunning ? styles.active : styles.passive}
+                            digitTxtStyle={this.state.secondTimerRunning ? styles.active : styles.passive}
+                            timeToShow={['M', 'S']}
+                            timeLabels={{ m: '', s: '' }}
+                            running={this.state.secondTimerRunning}
 
-
-
-                <View style={[{ flex: 1, backgroundColor: '#808080', borderRadius: 20 }]}>
-                    <View style={styles.text}>
-                        <Text>Erkan</Text>
+                        />
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </View>
         );
+    }
+
+    tickFirstTimer() {
+        this.setState({
+            firstTimerRunning: false,
+            secondTimerRunning: true
+        })
+    }
+
+    tickSecondTimer() {
+        this.setState({
+            firstTimerRunning: true,
+            secondTimerRunning: false
+        })
     }
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 0,
-        margin: 0,
-        display: 'flex'
-    },
-    text: {
-        flex: 2,
-        backgroundColor: '#808080',
-        borderRadius: 20,
-        padding: 10,
-        margin: 10,
-
-    },
-    text2: {
-        flex: 2,
-        backgroundColor: '#49D265',
-        padding: 10,
-        borderRadius: 20,
-        margin: 10
+    tabs: {
+        flex: 1
     },
     settings: {
         flex: 0.5,
-        backgroundColor: '#013CFE',
+        backgroundColor: '#444444',
         padding: 10
+    },
+    active: {
+        color: '#FFFFFF',
+        backgroundColor: '#FFA500',
+        textAlign: 'center'
+    },
+    passive: {
+        color: '#000000',
+        backgroundColor: '#9C9C9C',
+        textAlign: 'center'
+    },
+    rotate180: {
+        transform: [{ rotate: '180deg' }]
     }
+
 });
